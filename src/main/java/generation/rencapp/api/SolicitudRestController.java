cc5c51abe87b2fea2d3525f6a7e06bceb00f150b
mendriLg <eleo.ligu@gmail.com>
2024-10-29 23:01:12 -0300

    actualizacion de base de datos matibkn 29/10/24

--- src/main/java/generation/rencapp/api/SolicitudRestController.java
+++ src/main/java/generation/rencapp/api/SolicitudRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/solicitudes")
+@CrossOrigin("*")
 public class SolicitudRestController {
 
     @Autowired
--- src/main/java/generation/rencapp/api/TramiteRestController.java
+++ src/main/java/generation/rencapp/api/TramiteRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tramites")
+@CrossOrigin("*")
 public class TramiteRestController {
 
     @Autowired
